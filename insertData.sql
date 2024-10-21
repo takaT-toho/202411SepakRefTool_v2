8f59100df878b5d18fc605d1d14a8b2a02b20e4f
takaT-toho <tk06.ik09.ksk@gmail.com>
2024-10-22 06:53:52 +0900

    update init sql

---
+++ insertData.sql
@@ -0,0 +1,254 @@
+
+INSERT INTO COURT VALUES (3, 'C', '12343', NULL, 0);
+INSERT INTO COURT VALUES (4, 'D', '12344', NULL, 0);
+INSERT INTO COURT VALUES (5, 'E', '12345', NULL, 0);
+INSERT INTO COURT VALUES (6, 'F', '12346', NULL, 0);
+INSERT INTO COURT VALUES (7, 'G', '12347', NULL, 0);
+INSERT INTO COURT VALUES (8, 'H', '12348', NULL, 0);
+
+INSERT INTO TEAM VALUES (101, 'TAK-TAK', '北海道太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (102, 'SepakTakrawClub\”RAPORA\”', '北海道太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (103, '函館セパタクロークラブ', '北海道太郎', 'xxxx@xxxxxx.com');
+
+INSERT INTO TEAM VALUES (201, '東北大学セパタクロークラブ', '東北太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (202, '陸前高田セパタクロー協会', '東北太郎', 'xxxx@xxxxxx.com');
+
+INSERT INTO TEAM VALUES (301, '千葉大学セパタクロークラブ', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (302, '日本体育大学セパタクロー部', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (303, '亜細亜大学セパタクロー部', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (304, '東京大学セパタクロークラブ', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (305, '東京工業大学セパタクロークラブ', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (306, '慶應セパタクロークラブ', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (307, '欅セパタクロークラブ', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (308, '横濱セパタクロークラブ', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (309, 'A.S.WAKABA', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (310, 'ふじみセパタクロークラブ', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (311, '大山セパタクロークラブ', '関東太郎', 'xxxx@xxxxxx.com');
+INSERT INTO TEAM VALUES (312, 'AK.tippler', '関東太郎', 'xxxx@xxxxxx.com');
+
+INSERT INTO TEAM VALUES (401, '下越セパタクロー倶楽部', '北陸甲信', 'xxxx@xxxxxx.com');
+
+INSERT INTO TEAM VALUES (601, '阪神酒販', '近畿太郎', 'xxxx@xxxxxx.com');
+
+
+INSERT INTO REGU VALUES (10100, 101, '蝦夷ボリック', '蝦夷ボリック', 'アスリート');
+INSERT INTO REGU VALUES (10101, 101, '花火師たち', '花火師たち', 'アスリート');
+INSERT INTO REGU VALUES (10102, 101, 'たくらぽしー', 'たくらぽしー', 'アスリート');
+INSERT INTO REGU VALUES (10103, 101, 'MiYATAK', 'MiYATAK', 'アスリート');
+INSERT INTO REGU VALUES (10104, 101, '最もプリミティブ', 'もっぷり', 'エンジョイ');
+INSERT INTO REGU VALUES (10105, 101, 'セブン店員', 'セブン店員', 'エンジョイ');
+INSERT INTO REGU VALUES (10106, 101, 'ツジラブ', 'ツジラブ', 'エンジョイ');
+INSERT INTO REGU VALUES (10107, 101, 'P活6男2女', 'P活6男2女', 'エンジョイ');
+INSERT INTO REGU VALUES (10108, 101, '拓拓', '拓拓', 'エンジョイ');
+
+INSERT INTO REGU VALUES (10200, 102, 'ハラニシの仲間達', 'ハラニシの仲間達', 'エンジョイ');
+INSERT INTO REGU VALUES (10201, 102, 'ラブチャオズ家族', 'ラブチャオズ家族', 'エンジョイ');
+INSERT INTO REGU VALUES (10202, 102, '爆盛りハルヨシン', '爆盛りハルヨシン', 'エンジョイ');
+INSERT INTO REGU VALUES (10203, 102, 'むりりんのことかー！', 'むりりん', 'エンジョイ');
+INSERT INTO REGU VALUES (10204, 102, '田舎もんオールスターズ', '田舎モンスター', 'エンジョイ');
+INSERT INTO REGU VALUES (10205, 102, '和解', '和解', 'エンジョイ');
+INSERT INTO REGU VALUES (10206, 102, '優勝あん泰', '優勝あん泰', 'エンジョイ');
+INSERT INTO REGU VALUES (10207, 102, '結局フィジカル', '結局フィジカル', 'アスリート');
+
+INSERT INTO REGU VALUES (10300, 103, 'ナポリタン', 'ナポリタン', 'エンジョイ');
+
+INSERT INTO REGU VALUES (20100, 201, 'チーム清楚系', 'チーム清楚系', 'エンジョイ');
+INSERT INTO REGU VALUES (20101, 201, 'あやぼるちぃ', 'あやぼるちぃ', 'エンジョイ');
+INSERT INTO REGU VALUES (20102, 201, '宮城a', '宮城a', 'エンジョイ');
+INSERT INTO REGU VALUES (20103, 201, '司（仮）', '司（仮）', 'エンジョイ');
+INSERT INTO REGU VALUES (20104, 201, 'ちちびでぶ', 'ちちびでぶ', 'エンジョイ');
+INSERT INTO REGU VALUES (20105, 201, '関東支部E', '関東支部E', 'エンジョイ');
+INSERT INTO REGU VALUES (20106, 201, '宮城d', '宮城d', 'エンジョイ');
+INSERT INTO REGU VALUES (20107, 201, '宮城e', '宮城e', 'エンジョイ');
+INSERT INTO REGU VALUES (20108, 201, '宮城b', '宮城b', 'エンジョイ');
+INSERT INTO REGU VALUES (20109, 201, 'アボノミクス', 'アボノミクス', 'エンジョイ');
+INSERT INTO REGU VALUES (20110, 201, '♠︎3返し', '♠︎3返し', 'アスリート');
+INSERT INTO REGU VALUES (20111, 201, '負けたら叫びます', '負けたら叫びます', 'アスリート');
+INSERT INTO REGU VALUES (20112, 201, '関東支部A', '関東支部A', 'アスリート');
+INSERT INTO REGU VALUES (20113, 201, 'あぼの仇', 'あぼの仇', 'アスリート');
+INSERT INTO REGU VALUES (20114, 201, 'はやけいけい', 'はやけいけい', 'アスリート');
+INSERT INTO REGU VALUES (20115, 201, '宮城A', '宮城A', 'アスリート');
+INSERT INTO REGU VALUES (20116, 201, '宮城B', '宮城B', 'アスリート');
+INSERT INTO REGU VALUES (20117, 201, 'インフルエンザB', 'インフルエンザB', 'アスリート');
+
+INSERT INTO REGU VALUES (20200, 202, '順天堂大学セパタクロー部', '順大セパ部', 'エンジョイ');
+
+INSERT INTO REGU VALUES (30100, 301, 'エンジタクロー', 'エンジ', 'アスリート');
+INSERT INTO REGU VALUES (30101, 301, '法政経', '法政経', 'アスリート');
+INSERT INTO REGU VALUES (30102, 301, '千葉大学第三学年', '千葉大三年', 'アスリート');
+INSERT INTO REGU VALUES (30103, 301, '最強激弱', '最強激弱', 'エンジョイ');
+INSERT INTO REGU VALUES (30104, 301, 'はやてまかせ', 'はやてまかせ', 'エンジョイ');
+INSERT INTO REGU VALUES (30105, 301, 'イナズマイレブン2', 'イナイレ2', 'エンジョイ');
+INSERT INTO REGU VALUES (30106, 301, '仲良し3人組☆', '仲良し3人組', 'エンジョイ');
+INSERT INTO REGU VALUES (30107, 301, 'のぶサンズ', 'のぶサンズ', 'エンジョイ');
+INSERT INTO REGU VALUES (30108, 301, '田んぼマジカルモルモッツア', '田んぼ', 'エンジョイ');
+INSERT INTO REGU VALUES (30109, 301, 'チーム学連', '学連', 'エンジョイ');
+
+INSERT INTO REGU VALUES (30200, 302, '日本体育大学A', '日体A', 'アスリート');
+INSERT INTO REGU VALUES (30201, 302, '日本体育大学B', '日体B', 'アスリート');
+INSERT INTO REGU VALUES (30202, 302, '日本体育大学C', '日体C', 'アスリート');
+INSERT INTO REGU VALUES (30203, 302, '日本体育大学O', '日体O', 'アスリート');
+
+INSERT INTO REGU VALUES (30300, 303, 'アップデートしよ？', 'アップデート', 'アスリート');
+INSERT INTO REGU VALUES (30301, 303, '亜大フィフス', 'フィフス', 'アスリート');
+INSERT INTO REGU VALUES (30302, 303, '亜大シックス', 'シックス', 'アスリート');
+INSERT INTO REGU VALUES (30303, 303, 'コバセイ', 'コバセイ', 'アスリート');
+INSERT INTO REGU VALUES (30304, 303, '海亀ブラザーズ', '海亀', 'エンジョイ');
+INSERT INTO REGU VALUES (30305, 303, 'ローレンスといっしょ', 'ロレと', 'エンジョイ');
+INSERT INTO REGU VALUES (30306, 303, 'びっくりドンキーラバーズ', 'びくドン', 'エンジョイ');
+
+INSERT INTO REGU VALUES (30400, 304, '逆さま笹かま', '笹かま', 'エンジョイ');
+
+INSERT INTO REGU VALUES (30500, 305, 'SCi TOKYO', 'SCiTOKYO', 'アスリート');
+INSERT INTO REGU VALUES (30501, 305, '材料系+α', '材料系+α', 'アスリート');
+INSERT INTO REGU VALUES (30502, 305, 'ブル・プリモ', 'ブル・プリモ', 'アスリート');
+INSERT INTO REGU VALUES (30503, 305, 'Science Shogo', 'SciShogo', 'エンジョイ');
+
+INSERT INTO REGU VALUES (30600, 306, '慶應義塾大学Aレグ', '慶應A', 'アスリート');
+INSERT INTO REGU VALUES (30601, 306, '慶應義塾大学Bレグ', '慶應B', 'アスリート');
+INSERT INTO REGU VALUES (30602, 306, '慶應義塾大学Cレグ', '慶應C', 'アスリート');
+INSERT INTO REGU VALUES (30603, 306, '慶應義塾大学Dレグ', '慶應D', 'アスリート');
+
+INSERT INTO REGU VALUES (30700, 307, '欅A', '欅A', 'アスリート');
+INSERT INTO REGU VALUES (30701, 307, '欅B', '欅B', 'アスリート');
+INSERT INTO REGU VALUES (30702, 307, '珍苗字', '珍苗字', 'エンジョイ');
+
+INSERT INTO REGU VALUES (30800, 308, '横濱A', '横濱A', 'アスリート');
+
+INSERT INTO REGU VALUES (30900, 309, 'A.S.WAKABA Aレグ', 'WAKABA A', 'エンジョイ');
+INSERT INTO REGU VALUES (30901, 309, 'A.S.WAKABA Bレグ', 'WAKABA B', 'エンジョイ');
+
+INSERT INTO REGU VALUES (31000, 310, 'ふじみ女子', 'ふじみ女子', 'エンジョイ');
+
+INSERT INTO REGU VALUES (31100, 311, '大山たぬき', '大山たぬき', 'エンジョイ');
+
+INSERT INTO REGU VALUES (31200, 312, 'AK.tippler', 'AK', 'アスリート');
+
+INSERT INTO REGU VALUES (40100, 401, '下越セパ', '下越セパ', 'エンジョイ');
+
+INSERT INTO REGU VALUES (60100, 601, '阪神酒販first', '阪神酒販', 'アスリート');
+
+INSERT INTO REGU VALUES (90010, 900, '1A勝者', '1A勝者', 'アスリート');
+INSERT INTO REGU VALUES (90011, 900, '1A敗者', '1A敗者', 'アスリート');
+INSERT INTO REGU VALUES (90012, 900, '1B勝者', '1B勝者', 'アスリート');
+INSERT INTO REGU VALUES (90013, 900, '1B敗者', '1B敗者', 'アスリート');
+INSERT INTO REGU VALUES (90014, 900, '1C勝者', '1C勝者', 'アスリート');
+INSERT INTO REGU VALUES (90015, 900, '1C敗者', '1C敗者', 'アスリート');
+INSERT INTO REGU VALUES (90016, 900, '1D勝者', '1D勝者', 'アスリート');
+INSERT INTO REGU VALUES (90017, 900, '1D敗者', '1D敗者', 'アスリート');
+INSERT INTO REGU VALUES (90018, 900, '1E勝者', '1E勝者', 'アスリート');
+INSERT INTO REGU VALUES (90019, 900, '1E敗者', '1E敗者', 'アスリート');
+INSERT INTO REGU VALUES (90020, 900, '1F勝者', '1F勝者', 'アスリート');
+INSERT INTO REGU VALUES (90021, 900, '1F敗者', '1F敗者', 'アスリート');
+INSERT INTO REGU VALUES (90022, 900, '1G勝者', '1G勝者', 'アスリート');
+INSERT INTO REGU VALUES (90023, 900, '1G敗者', '1G敗者', 'アスリート');
+INSERT INTO REGU VALUES (90024, 900, '1H勝者', '1H勝者', 'アスリート');
+INSERT INTO REGU VALUES (90025, 900, '1H敗者', '1H敗者', 'アスリート');
+
+INSERT INTO REGU VALUES (90088, 900, '5H勝者', '5H勝者', 'アスリート');
+INSERT INTO REGU VALUES (90093, 900, '6D勝者', '6D勝者', 'アスリート');
+
+INSERT INTO gameDetail VALUES (101, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (102, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (103, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (104, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (105, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (106, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (107, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (108, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+    
+INSERT INTO gameDetail VALUES (507, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (508, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (601, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (602, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (603, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (604, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (605, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (606, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+
+INSERT INTO gameDetail VALUES (907, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+INSERT INTO gameDetail VALUES (908, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+
+INSERT INTO gameDetail VALUES (9999, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0,
+    1, 5, 1, 5, 2, 2, FALSE, 0);
+
+
+INSERT INTO GAME VALUES (
+101, '1A', '123411', '#1', '10:30', 1, 20101, 30107, 30901, NULL, 1,
+507, 601, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+102, '1B', '123411', '#1', '10:30', 2, 10202, 20104, 20106, NULL, 1,
+507, 601, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+103, '1C', '123411', '#1', '10:30', 3, 10105, 10201, 20100, NULL, 1,
+508, 602, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+104, '1D', '123411', '#1', '10:30', 4, 10106, 30305, 10300, NULL, 1,
+907, 602, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+105, '1E', '123411', '#1', '10:30', 5, 30105, 20109, 10108, NULL, 1,
+603, 605, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+106, '1F', '123411', '#1', '10:30', 6, 10200, 31100, 30109, NULL, 1,
+603, 605, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+107, '1G', '123411', '#1', '10:30', 7, 30108, 20103, 31000, NULL, 1,
+604, 606, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+108, '1H', '123411', '#1', '10:30', 8, 30306, 10204, 20200, NULL, 1,
+908, 606, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+
+INSERT INTO GAME VALUES (
+507, '5G', '123411', '#5', '12:50', 7, 90010, 90012, 30202, NULL, 1,
+9999, 9999, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+508, '5H', '123411', '#5', '12:50', 8, 30106, 90014, 31200, NULL, 1,
+9999, 9999, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+601, '6A', '123411', '#6', '13:25', 1, 90010, 90012, 30106, NULL, 1,
+9999, 9999, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+602, '6B', '123411', '#6', '13:25', 2, 90015, 90017, 30203, NULL, 1,
+9999, 9999, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+603, '6C', '123411', '#6', '13:25', 3, 90018, 90020, 30701, NULL, 1,
+9999, 9999, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+604, '6D', '123411', '#6', '13:25', 4, 30900, 90022, 20116, NULL, 1,
+9999, 9999, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+605, '6E', '123411', '#6', '13:25', 5, 90019, 90021, 10102, NULL, 1,
+9999, 9999, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+606, '6F', '123411', '#6', '13:25', 6, 90023, 90025, 20113, NULL, 1,
+9999, 9999, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+
+
+INSERT INTO GAME VALUES (
+907, '9G', '123411', '#9', '15:10', 7, 90088, 90016, 20112, NULL, 1,
+9999, 9999, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+INSERT INTO GAME VALUES (
+908, '9H', '123411', '#9', '15:10', 8, 90093, 90024, 30201, NULL, 1,
+9999, 9999, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
+
+INSERT INTO GAME VALUES (
+9999, '1G', '123411', '#5', '10:30', 7, 90011, 90013, 90001, NULL, 1,
+908, 606, FALSE, FALSE, FALSE, FALSE, FALSE, 0, 0, NULL, NULL, NULL);
